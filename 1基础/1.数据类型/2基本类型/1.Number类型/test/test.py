12217368daf52232941cf214ce5730726807f00f
zyk <2126483838@qq.com>
2025-03-19 18:41:04 +0800

    不可变类型-Number\String\tuple

---
+++ 1基础/1.数据类型/2基本类型/1.Number类型/test/test.py
@@ -0,0 +1,7 @@
+a, b, c, d = 20, 5.5, True, 4+3j
+print(type(a), type(b), type(c), type(d))
+print(a, b, c, d)
+flag=isinstance(a, int)
+print(flag)
+i=10;
+print(i.bit_length())# 明确了所有类型都是类！
---
+++ 1基础/1.数据类型/2基本类型/2.String类型/test/test.py
@@ -0,0 +1,5 @@
+str="1234567890"
+print(str[0:3])
+# 2到倒数第三个
+print(str[2:-3])
+print(str[0:3] + "abc")
